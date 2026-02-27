2be1c85ec1495cfcda7707dced6cad23f7e86a9e
zaki <yousif>
2026-02-27 19:38:51 +0200

    fix:dashboard on login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -16,7 +16,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View|RedirectResponse
     {
-    
+
         return view('auth.login-register');
     }
 
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('dashboard.auth', absolute: false));
     }
 
     /**
